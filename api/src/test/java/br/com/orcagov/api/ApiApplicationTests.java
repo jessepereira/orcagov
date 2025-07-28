6ccfe53d536093c193bacc13eba8fdc014ad1d50
nobuzaga <nobuzaga@DESENV-UV196IP.localdomain>
2025-07-27 23:18:46 -0300

    estrutura inicial do projeto

---
+++ api/src/test/java/br/com/orcagov/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.orcagov.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
